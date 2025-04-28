af84f214ec27ec3d2fcd4991f47233f2366314e8
GoldPapaya <lucasdcrawford32@gmail.com>
2025-04-28 13:45:56 -0400

    first commit

---
+++ visualizer.py
@@ -0,0 +1,69 @@
+import netCDF4 as nc
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+
+nc_file = 'C:/Users/lucas/.vscode/swe/visualizer_input/swemap/2016_2017/20170201_northern_hemisphere_swe_0.25grid.nc'
+shp_file = 'C:/Users/lucas/.vscode/swe/visualizer_input/mask/southernontariomask.shp'  # Optional
+prov_file = 'C:/Users/lucas/.vscode/swe/visualizer_input/bounds/provinceboundaries.shp'
+
+dataset = nc.Dataset(nc_file, 'r')
+x = dataset.variables['x'][:]
+y = dataset.variables['y'][:]
+swe = dataset.variables['swe'][:]
+
+# Lambert Azimuthal Equal Area projection
+proj = ccrs.LambertAzimuthalEqualArea(
+    central_longitude=0,
+    central_latitude=90,
+    false_easting=0,
+    false_northing=0,
+    globe=ccrs.Globe(semimajor_axis=6371228.0, semiminor_axis=6371228.0)
+)
+
+plt.figure(figsize=(12, 8))
+ax = plt.axes(projection=proj)
+ax.set_global()
+ax.coastlines()
+
+mesh = ax.pcolormesh(x, y, swe, cmap='Blues', zorder=1)
+plt.colorbar(mesh, label='Snow Water Equivalent (mm)')
+
+# Overlay shapefile
+try:
+    shp_reader = shpreader.Reader(shp_file)
+    shapes = list(shp_reader.geometries())
+    print(f"Loaded southernontariomask.shp")
+    ax.add_geometries(
+        shapes,
+        crs=proj,
+        edgecolor='red',
+        facecolor='none',
+        linewidth=1.5,
+        zorder=2
+    )
+except ValueError as e:
+    print(f"Shapefile error (southernontariomask): {e}")
+
+# Overlay borders
+try:
+    shp_reader = shpreader.Reader(prov_file)
+    shapes = list(shp_reader.geometries())
+    print(f"Loaded provinceboundaries.shp")
+    ax.add_geometries(
+        shapes,
+        crs=proj,
+        edgecolor='black',
+        alpha=0.1,
+        facecolor='none',
+        linewidth=1,
+        zorder=2
+    )
+except ValueError as e:
+    print(f"Shapefile error (provinceboundaries): {e}")
+
+plt.title('SWE Map (Feb 2, 2017)')
+plt.show()
+
+dataset.close()
